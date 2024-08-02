cc6dc852b904736f27124688f2470dd27d9b572c
Bartłomiej Sikora <sbartek50@gmail.com>
2024-08-02 19:02:43 +0200

    Created the Button component.

---
+++ components/ui/button/Button.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import ButtonProps from './button.types'
+import style from './button.module.scss'
+
+import Image from 'next/image'
+
+const Button = ({ text, $icon, $showArrow, onClick, $variant, $size }: ButtonProps) => {
+    const getBtnClasses = () => {
+        let newClasses = style.btn
+
+        switch ($variant) {
+            case 'sec': newClasses += ' ' + style.btnSec
+        }
+
+        if ($size === 'big') newClasses += ' ' + style.btnBig
+
+        return newClasses
+    }
+
+    const [btnClasses, setBtnClasses] = useState<string>(getBtnClasses())
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(() => setBtnClasses(getBtnClasses()), [$variant, $size])
+
+    return (
+        <button className={btnClasses} onClick={onClick}>
+            {
+                $icon && <Image src={$icon} width={25} height={25} alt='Ikona przycisku' />
+            }
+
+            {text}
+
+            {
+                $showArrow && <Image src='/icons/arrow-down.svg' width={25} height={25} alt='Rozwiń...' />
+            }
+        </button>
+    )
+}
+
+export default Button
