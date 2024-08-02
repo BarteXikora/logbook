24e807be9fa329b288928a046661b240fbc99bb6
Bartłomiej Sikora <sbartek50@gmail.com>
2024-08-02 18:16:30 +0200

    Created the theme index file exporting combined body classes.

---
+++ theme/index.ts
@@ -0,0 +1,6 @@
+import { crimsonPro, raleway } from './fonts'
+import style from './global.module.scss'
+
+const bodyClass = `${crimsonPro.variable} ${raleway.variable} ${style.root}`
+
+export default bodyClass
