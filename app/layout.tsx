0f2eaa759987e38975791187a43ab8ff6df10a44
Bartłomiej Sikora <sbartek50@gmail.com>
2024-08-04 11:19:34 +0200

    Placed the Navbar in the root layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react'
 import type { Metadata } from 'next'
 
+import Navbar from '@components/layout/navbar/Navbar'
+
 import bodyClass from '@theme'
 
 export const metadata: Metadata = { title: 'logbook' }
@@ -8,7 +10,11 @@ export const metadata: Metadata = { title: 'logbook' }
 const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
     <html lang="pl">
-      <body className={bodyClass}>{children}</body>
+      <body className={bodyClass}>
+        <Navbar />
+
+        {children}
+      </body>
     </html>
   )
 }
