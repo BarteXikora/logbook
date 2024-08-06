e2f6c03f9a3181546ce0159e69d91709dbed85f9
Bartłomiej Sikora <sbartek50@gmail.com>
2024-08-06 10:42:01 +0200

    Moved the navigation to the separate component.

--- components/layout/navbar/Navbar.tsx
+++ components/layout/navbar/Navbar.tsx
@@ -2,35 +2,16 @@ import Link from 'next/link'
 import Button from '@components/ui/button/Button'
 
 import Logo from './logo/Logo'
+import Navigation from './navigation/Navigation'
 
 import style from './navbar.module.scss'
 
 const Navbar = () => {
-    const __isAddedToday = false
-
     return (
         <header className={style.navbar}>
             <Logo />
 
-            <nav className={style.navigation}>
-                <div className={style.hamburgerBtn}>
-                    <Button text='' $icon='/icons/hamburger.svg' $size='big' />
-                </div>
-
-                <div className={style.navLinks}>
-                    <Link href='/'>Pulpit</Link>
-                    <Link href='/'>Kalendarz</Link>
-                    <Link href='/'>Wpisy</Link>
-
-                    {
-                        !__isAddedToday && <Button $variant='sec' text='Dodaj dzisiejszy wpis' />
-                    }
-                </div>
-
-                <div className={style.userBtn}>
-                    <Button $variant='sec' text='Nazwa użytkownika' $icon='/icons/user.svg' $size='big' $showArrow />
-                </div>
-            </nav>
+            <Navigation />
         </header>
     )
 }
