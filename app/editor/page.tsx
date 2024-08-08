b39ed5e904e3f0949ff81c897e792defba22c118
Bartłomiej Sikora <sbartek50@gmail.com>
2024-08-08 16:16:16 +0200

    Placed the EditorPaper component in the editor page.

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,12 +1,19 @@
 import EditorTools from '@components/layout/editorTools/EditorTools'
+import EditorPaper from '@components/layout/editorPaper/EdtorPaper'
 
 import style from './page.module.scss'
 
 const Editor = () => {
     return (
-        <div className={style.editorContainer}>
-            <EditorTools />
-        </div>
+        <>
+            <div className={style.editorToolsContainer}>
+                <EditorTools />
+            </div>
+
+            <div className={style.editorPaperContainer}>
+                <EditorPaper />
+            </div>
+        </>
     )
 }
 
