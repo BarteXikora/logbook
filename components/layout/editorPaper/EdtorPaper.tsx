8a5aed333fdb2ec8bb7f24acfb2838fda3438fc5
Bartłomiej Sikora <sbartek50@gmail.com>
2024-08-08 16:15:20 +0200

    Created the EditorPaper component.

---
+++ components/layout/editorPaper/EdtorPaper.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import style from './editorPaper.module.scss'
+
+const EdtorPaper = () => {
+    return (
+        <div className={style.editorPaperContainer}>
+            <main className={style.editorPaper}></main>
+        </div>
+    )
+}
+
+export default EdtorPaper
