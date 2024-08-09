4e47316af2d28156021f0a321068a3049121bf27
Bartłomiej Sikora <sbartek50@gmail.com>
2024-08-09 10:31:18 +0200

    Created the Element component.

---
+++ components/layout/editorPaper/element/Element.tsx
@@ -0,0 +1,37 @@
+import { FocusEvent } from 'react'
+import ElementProps from './element.types'
+import style from './element.module.scss'
+
+const Element = (element: ElementProps) => {
+    const { type } = element
+
+    // TEMPORARY:
+    if (type === 'separator') return <hr />
+    if (type === 'quote') return <>quote</>
+    //
+
+    const { state, alignment } = element
+
+    const handleBlur = (e: FocusEvent) => {
+        const relatedTarget = e.relatedTarget as HTMLElement
+        const currentTarget = e.currentTarget as HTMLElement
+
+        if (relatedTarget && relatedTarget.dataset.donotblur) {
+            e.preventDefault()
+            currentTarget.focus()
+        }
+
+        state.setValue(e.currentTarget.innerHTML)
+    }
+
+    return (
+        <div
+            contentEditable suppressContentEditableWarning
+            className={`${style.elementInput} ${style['elementType-' + type]} ${style['elementAlign-' + alignment]}`}
+            onBlur={handleBlur}
+            dangerouslySetInnerHTML={{ __html: state.value }}
+        />
+    )
+}
+
+export default Element
