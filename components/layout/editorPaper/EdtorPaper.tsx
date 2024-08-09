6e799ccf5e8d039319c1de9445b270c280079f3a
Bartłomiej Sikora <sbartek50@gmail.com>
2024-08-09 09:33:08 +0200

    Placed the DatePill component in the EditorPaper component.

--- components/layout/editorPaper/EdtorPaper.tsx
+++ components/layout/editorPaper/EdtorPaper.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+'use client'
+
+import DatePill from './datePill/DatePill'
 
 import style from './editorPaper.module.scss'
 
 const EdtorPaper = () => {
     return (
         <div className={style.editorPaperContainer}>
-            <main className={style.editorPaper}></main>
+            <main className={style.editorPaper}>
+                <DatePill date='9 sierpnia 2024' />
+            </main>
         </div>
     )
 }
