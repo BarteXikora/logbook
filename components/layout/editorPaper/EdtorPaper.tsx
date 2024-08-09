87ed47600b099600019d6b770fcefbdad3b782a7
Bartłomiej Sikora <sbartek50@gmail.com>
2024-08-09 17:08:53 +0200

    Created functionality of adding new element on entr click to the EditorPaper component.

--- components/layout/editorPaper/EdtorPaper.tsx
+++ components/layout/editorPaper/EdtorPaper.tsx
@@ -1,8 +1,9 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '@store/store'
-import { editElement } from '@store/editorSlice/editorSlice'
+import { addElement, editElement } from '@store/editorSlice/editorSlice'
 import ElementProps from './element/element.types'
 
 import DatePill from './datePill/DatePill'
@@ -13,10 +14,33 @@ const EdtorPaper = () => {
     const dispatch = useDispatch()
     const { date, content } = useSelector((state: RootState) => state)
 
+    const [focusNext, setFocusNext] = useState<boolean>(false)
+
+    useEffect(() => {
+        if (focusNext) {
+            const focusableElements = Array.from(document.querySelectorAll('div')) as HTMLElement[]
+            const currentIndex = focusableElements.indexOf(document.activeElement as HTMLElement)
+
+            if (currentIndex >= 0 && currentIndex < focusableElements.length) {
+                const nextElement = focusableElements[currentIndex + 1]
+                nextElement.focus()
+            }
+
+            setFocusNext(false)
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [content])
+
     const handleSetValue = (elementN: number, value: string) => {
         if (!('content' in content[elementN])) return
 
         dispatch(editElement({ elementN, newValue: { ...content[elementN], content: value } }))
+    }
+
+    const handleNextElement = (position: number) => {
+        setFocusNext(true)
+        dispatch(addElement(position))
     }
 
     return (
@@ -31,10 +55,12 @@ const EdtorPaper = () => {
                         if ('content' in element)
                             elementProps = {
                                 ...element,
-                                state: { value: element.content, setValue: (value: string) => handleSetValue(n, value) }
+                                state: { value: element.content, setValue: (value: string) => handleSetValue(n, value) },
+                                nextElement: () => handleNextElement(n)
                             }
 
-                        else elementProps = { ...element }
+                        else
+                            elementProps = { ...element, nextElement: () => handleNextElement(n) }
 
                         return (
                             <Element key={n} {...elementProps} />
