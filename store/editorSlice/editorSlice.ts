ec66cf561f98570991727d171652d11b2ad43f17
Bartłomiej Sikora <sbartek50@gmail.com>
2024-08-09 17:06:21 +0200

    Created the addElement reducer.

--- store/editorSlice/editorSlice.ts
+++ store/editorSlice/editorSlice.ts
@@ -1,15 +1,17 @@
 import { createSlice } from '@reduxjs/toolkit'
 import initialState from './editorSliceInitialState'
 
+import addElementReducer from './reducers/addElement'
 import editElementReducer from './reducers/editElement'
 
 const editorSlice = createSlice({
     name: 'editor',
     initialState,
     reducers: {
+        addElement: addElementReducer,
         editElement: editElementReducer
     }
 })
 
 export default editorSlice.reducer
-export const { editElement } = editorSlice.actions
+export const { addElement, editElement } = editorSlice.actions
